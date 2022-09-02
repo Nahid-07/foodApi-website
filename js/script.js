293b34dc74c125f9feaa557d13b789dfed3c7f28
Nahid-07 <nahidasif1998@gmail.com>
2022-09-02 14:59:59 +0600

    UI implement and search food by name implemented

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const loadFoodData = (search) =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayFood(data.meals))
+}
+const displayFood = meals => {
+    const mealContainer = document.getElementById('food-container');
+    mealContainer.innerHTML = ``;
+    meals.forEach(element => {
+        console.log(element)
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        // adding food dynamicaly
+        mealDiv.innerHTML = `
+        <div class="card">
+            <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${element.strMeal}</h5>
+            <p class="card-text">${element.strInstructions.slice(0,200)}</p>
+            </div>
+        </div>
+        `;
+        mealContainer.appendChild(mealDiv)
+    })
+}
+const searchFood = () =>{
+    const searchfield = document.getElementById('search-field');
+    const searchInput = searchfield.value;
+    loadFoodData(searchInput)
+}
+loadFoodData('')
